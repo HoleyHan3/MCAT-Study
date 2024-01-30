7c6177217363e62f56bf27b128b4da251d20b88d
Han <hanzhang02@gmail.com>
2024-01-29 22:43:29 -0500

    s

--- pages/_2_Bio_Biochem.py
+++ pages/_2_Bio_Biochem.py
@@ -4,17 +4,23 @@ st.title("Biochemistry and Biology")
 st.write("This section covers topics related to biochemistry and biology.")
 st.write("Topics include molecular biology, cell biology, genetics, metabolism, and more.")
 
-st.header('Biology', divider='blue')
-st.subheader("Features:")
-st.write("- Detailed explanations of key concepts.")
-st.write("- Interactive diagrams and animations.")
-st.write("- Practice quizzes with instant feedback.")
-st.write("- Access to external resources and study guides.")
+col1, col2 = st.columns(2)
 
-st.header('Biochemistry', divider='gray') #colors: blue, green, orange, red, violet, gray/grey, rainbow.
-st.subheader("Features:")
-st.write("- Detailed explanations of key concepts.")
-st.write("- Interactive diagrams and animations.")
-st.write("- Practice quizzes with instant feedback.")
-st.write("- Access to external resources and study guides.")
+with col1:
+
+    st.header('Biology', divider='blue')
+    st.subheader("Features:")
+    st.write("- Detailed explanations of key concepts.")
+    st.write("- Interactive diagrams and animations.")
+    st.write("- Practice quizzes with instant feedback.")
+    st.write("- Access to external resources and study guides.")
+
+
+with col2:
+    st.header('Biochemistry', divider='gray') #colors: blue, green, orange, red, violet, gray/grey, rainbow.
+    st.subheader("Features:")
+    st.write("- Detailed explanations of key concepts.")
+    st.write("- Interactive diagrams and animations.")
+    st.write("- Practice quizzes with instant feedback.")
+    st.write("- Access to external resources and study guides.")
 
