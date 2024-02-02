8cb605aa218390c39de84c44092cd74373ed8478
Han <hanzhang02@gmail.com>
2024-02-02 16:26:23 -0500

    s

--- Home.py
+++ Home.py
@@ -3,15 +3,15 @@ import streamlit_authenticator as stauth
 from menu import menu
 
 # Initialize st.session_state.role to None
-if "role" not in st.session_state:
-    st.session_state.role = None
+#if "role" not in st.session_state:
+#    st.session_state.role = None
 
 # Retrieve the role from Session State to initialize the widget
-st.session_state._role = st.session_state.role
+#st.session_state._role = st.session_state.role
 
-def set_role():
+#def set_role():
     # Callback function to save the role selection to Session State
-    st.session_state.role = st.session_state._role
+#    st.session_state.role = st.session_state._role
 
 ## Selectbox to choose role
 #st.selectbox(
@@ -20,7 +20,9 @@ def set_role():
 #    key="_role",
 #    on_change=set_role,
 #)
-menu() # Render the dynamic menu!
+#menu() # Render the dynamic menu!
+
+#hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 
 
 def main():
