5af25b740f36ad27dfffe62c60c8158c96ddd5eb
Han <hanzhang02@gmail.com>
2024-02-02 18:32:22 -0500

    s

--- pages/about.py
+++ pages/about.py
@@ -1,12 +1,5 @@
 import streamlit as st
 import pandas as pd
-from menu import menu_with_redirect
-
-# Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
-
-#st.title("This page is available to all users")
-#st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
 
 st.title("About")
 st.write("This section provides information about the MCAT.")
