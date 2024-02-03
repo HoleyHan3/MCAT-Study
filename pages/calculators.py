10d8f3afcbb6c4551f3a999d6fb3afddb3885734
Han <hanzhang02@gmail.com>
2024-02-03 15:07:28 -0500

    d

--- pages/calculators.py
+++ pages/calculators.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from components.formula import FormulaManager
-from modules.menu import menu_with_redirect
+#from modules.menu import menu_with_redirect
 
 # Redirect to app.py if not logged in, otherwise show the navigation menu
-menu_with_redirect()
+#menu_with_redirect()
 
 #st.title("This page is available to all users")
 #st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
