2b1322b48eddb1022393dc651195c4c5be0a8fb4
Han <hanzhang02@gmail.com>
2024-02-03 13:01:58 -0500

    d

---
+++ pages/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Home")
+st.sidebar.markdown("# Home")
+
+st.title("About")
+st.write("This section provides information about the MCAT.")
+st.write("Learn about the app's features, development team, and how to provide feedback.")
+st.write("Stay updated with the latest news and announcements regarding the app.")
+
+st.header("About the MCAT")
+st.write("This section provides information about the MCAT.")
