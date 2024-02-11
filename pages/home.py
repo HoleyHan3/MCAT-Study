e8a73dc2d2a9822c8c6d671a4a9baf8a409bd041
Han <hanzhang02@gmail.com>
2024-02-11 09:17:25 -0500

    s

--- pages/home.py
+++ pages/home.py
@@ -1,18 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-#st.set_page_config(
-#    page_title="Home",
-#    page_icon="🧊",
-#    layout="wide",
-#    initial_sidebar_state="expanded",
-#    menu_items={
-#        'Get Help': 'https://www.extremelycoolapp.com/help',
-#        'Report a bug': "https://www.extremelycoolapp.com/bug",
-#        'About': "# This is a header. This is an *extremely* cool app!"
-#    }
-#)
-
 st.markdown("# Home")
 st.markdown(f"You are currently logged with the role of {st.session_state.role}.")  
 st.sidebar.markdown("# Home")
