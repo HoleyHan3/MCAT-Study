249841087dbc1f88e7ec79c7607e76b96c48ed57
Han <hanzhang02@gmail.com>
2024-02-11 09:52:10 -0500

    s

--- pages/Login.py
+++ pages/Login.py
@@ -3,10 +3,22 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from modules.auth import login_user, register_user
 from modules.password import save_password, verify_password
+import yaml
+from yaml.loader import SafeLoader
+
+with open('../streamlit/config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
 #Redirect to app.py if not logged in, otherwise show the navigation menu
 
-st.title("Login form")
 st.markdown(f"You are currently logged with the role of {st.session_state.role}.")
 
 # login.py
