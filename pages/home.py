38497eb98a81c8eb673ff1d9a74d8df9763cc851
Han <hanzhang02@gmail.com>
2024-02-11 09:08:52 -0500

    s

--- pages/home.py
+++ pages/home.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(
-    page_title="Home",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
-)
+#st.set_page_config(
+#    page_title="Home",
+#    page_icon="🧊",
+#    layout="wide",
+#    initial_sidebar_state="expanded",
+#    menu_items={
+#        'Get Help': 'https://www.extremelycoolapp.com/help',
+#        'Report a bug': "https://www.extremelycoolapp.com/bug",
+#        'About': "# This is a header. This is an *extremely* cool app!"
+#    }
+#)
 
 st.markdown("# Home")
 st.sidebar.markdown("# Home")
